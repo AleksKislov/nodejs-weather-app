5ae1b19953b4f89de244c3a90b33125f1cd0eeb6
AleksKislov <sinokislov@gmail.com>
2020-02-05 18:46:18 +0800

    Change lang into English

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = 'https://api.darksky.net/forecast/cac9705acd8399c07bc31ec62723fd43/' + latitude + ',' + longitude + '?lang=ru'
+    const url = 'https://api.darksky.net/forecast/cac9705acd8399c07bc31ec62723fd43/' + latitude + ',' + longitude + '?lang=en'
 
     request( { url, json: true } ,(error, { body }) => {
 
